6a2226a244b2758313b4875c592fcd5ff1da79a1
tomma <tommasorosso1@gmail.com>
2020-03-21 20:55:10 +0100

    github pages?

---
+++ docs/sw.js
@@ -0,0 +1,53 @@
+var CACHE_NAME = 'timer-cache-v1';
+var urlsToCache = [
+  '/',
+  '/main.css',
+  '/main.js',
+  '/when.mp3'
+];
+
+self.addEventListener('install', function(event) {
+    console.log('Service worker -> Install');
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Service worker -> Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        console.log('Service worker -> Cache hit');
+        // Cache hit - return response
+        if (response) {
+            return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+          // Check if we received a valid response
+          if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+          }
+
+          var responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+              .then(function(cache) {
+              cache.put(event.request, responseToCache);
+              });
+
+          console.log('Service worker -> Cache storing value');
+
+          return response;
+          }
+        );
+      })
+    );
+  });
+  
